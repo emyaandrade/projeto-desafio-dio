73155e8a3e0d10c73d0059da68977116cfb15fd4
Emilly Andrade <emilly.andrade.digital@gmail.com>
2025-07-02 15:11:47 -0300

    desafio finalizado

---
+++ README.md
@@ -0,0 +1,25 @@
+# Desafio Classificador de nível de Herói
+
+**O que deve ser utilizado**
+
+- Variáveis
+- Operadores
+- Laços de repetição
+- Estruturas de decisão
+
+## Objetivo
+
+Crie uma variável para armazenar o nome e a quantidade de experiência (XP) de um herói, depois utilize uma estrutura de decisão para apresentar alguma das mensagens abaixo:
+
+Se XP for menor que 1.000 = Ferro
+Se XP for entre 1.001 e 2.000 = Bronze
+Se XP for entre 2.001 e 5.000 = Prata Ouro
+Se XP for entre 5.001 e 8.000 = Platina Diamante
+Se XP for entre 8.001 e 9.000 = Ascendente
+Se XP for entre 9.001 e 10.000 = Imortal
+Se XP for maior ou igual a 10.001 = Radiante
+
+## Saída
+
+Ao final deve ser exibida uma mensagem:
+"O Herói de nome **{nome}** está no nível **{nível}**
--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log("Teste")
+//variáveis com informações dos heróis
+let nomeHeroi = "Escarlate";
+let quantidadeXP = 10.000;
+//variável para guardar o nivel do herói
+let nivelHeroi;
+
+//Descobrir XP e definir o nível do herói
+if (quantidadeXP < 1.000) {
+    nivelHeroi = "Ferro";
+} else if (quantidadeXP >= 1.001 && quantidadeXP <= 2.000){
+    nivelHeroi = "Bronze";
+} else if (quantidadeXP >= 2.001 && quantidadeXP <= 5.000){
+    nivelHeroi = "Prata";
+} else if (quantidadeXP >= 5.001 && quantidadeXP <= 7.000){
+    nivelHeroi = "Ouro";
+} else if (quantidadeXP >= 7.001 && quantidadeXP <= 8.000){
+    nivelHeroi = "Platina";
+} else if (quantidadeXP >= 8.001 && quantidadeXP <= 9.000){
+    nivelHeroi = "Ascendente";
+} else if (quantidadeXP >= 9.001 && quantidadeXP <= 10.000){
+    nivelHeroi = "Imortal";
+} else {
+    nivelHeroi = "Radiante";
+}
+
+console.log("O Herói de nome: " +nomeHeroi + "\nEstá no nível: " +nivelHeroi +" com " +quantidadeXP + "xp")
+
